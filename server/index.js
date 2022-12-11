938b243a42c0c23b2df223ba44f7ce0690f4068c
ange1beat <106840878+ange1beat@users.noreply.github.com>
2022-12-11 18:49:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const http = require('http').Server(app);
-const PORT = 4000
+const PORT = process.env.PORT || 5000
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "*"
     }
 });
 
