02e124b599100a548a9a739403c950294edccca0
ange1beat <106840878+ange1beat@users.noreply.github.com>
2022-12-11 15:41:23 +0300

    Update useChat.js

--- client/letterchat/src/useChat.js
+++ client/letterchat/src/useChat.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "http://3.75.158.163:10000";
+const SOCKET_SERVER_URL = "http://localhost";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
